78a67167e27524e85e2cea892bd827f3bc4728a2
ahmethakanbesel <ahmethakan.besel@gmail.com>
2023-10-03 14:33:22 +0300

    feat: add migration command

--- main.go
+++ main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/ahmethakanbesel/finance-api/pkg/routes"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Admin UI
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
 
 	// serves static files from the provided public dir (if exists)
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
