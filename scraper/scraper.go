22dbc591b94203b37756b7c686238b244c865536
ahmethakanbesel <ahmethakan.besel@gmail.com>
2023-12-29 16:53:57 +0300

    scraper: remove unwanted comments

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -10,6 +10,4 @@ const (
 
 type Scraper interface {
 	GetSymbolData(symbol string, startDate, endDate time.Time) (<-chan *SymbolPrice, error)
-	//scrapeChunk(symbol string, startDate time.Time, endDate time.Time) (<-chan SymbolPrice, error)
-	//getChunkSize() int
 }
